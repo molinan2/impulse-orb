e3ab9a04362dc4122192f6c3e79244fd889b2f12
molinan2 <molinan2@gmail.com>
2016-05-17 14:54:35 +0200

    First commit

---
+++ desktop/src/com/jmolina/orb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jmolina.orb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jmolina.orb.Orb;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Orb(), config);
+	}
+}
---
+++ html/src/com/jmolina/orb/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.jmolina.orb.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.jmolina.orb.Orb;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Orb();
+        }
+}
