805ea8bacacb40d1f31814d97c916595bd1037b9
molinan2 <molinan2@gmail.com>
2016-05-22 17:38:56 +0200

    LevelCardWidget and LevelSelectScreen.

--- core/src/com/jmolina/orb/screens/LevelSelectScreen.java
+++ core/src/com/jmolina/orb/screens/LevelSelectScreen.java
@@ -5,46 +5,43 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.jmolina.orb.widgets.LevelCardWidget;
 
 public class LevelSelectScreen extends MenuScreen {
 
-    // private LevelCard level1;
-    // private LevelCard level2;
-    // private LevelCard level3;
-    // private LevelCard level4;
-    // private LevelCard level5;
-    // private LevelCard level6;
-
-    private Texture levelTexture;
-    private Image level1;
-    private Image level2;
-    private Image level3;
-    private Image level4;
+    private LevelCardWidget level1;
+    private LevelCardWidget level2;
+    private LevelCardWidget level3;
+    private LevelCardWidget level4;
+    private Texture level1CoverTexture;
+    private Texture level1TitleTexture;
+    private Texture level1BestTexture;
+    private Texture level1WorldTexture;
 
     public LevelSelectScreen() {
         super();
 
-        levelTexture = new Texture(Gdx.files.internal("level.png"));
-        level1 = new Image(new TextureRegionDrawable(new TextureRegion(levelTexture)));
-        setPositionRelative(0.083f, 0.65f, level1);
-        getStage().addActor(level1);
+        level1CoverTexture = new Texture(Gdx.files.internal("card_cover.png"));
+        level1TitleTexture = new Texture(Gdx.files.internal("card_title.png"));
+        level1BestTexture = new Texture(Gdx.files.internal("card_best.png"));
+        level1WorldTexture = new Texture(Gdx.files.internal("card_world.png"));
+        level1 = new LevelCardWidget(level1CoverTexture, level1TitleTexture, level1BestTexture, level1WorldTexture);
+        level2 = new LevelCardWidget(level1CoverTexture, level1TitleTexture, level1BestTexture, level1WorldTexture);
+        level3 = new LevelCardWidget(level1CoverTexture, level1TitleTexture, level1BestTexture, level1WorldTexture);
+        level4 = new LevelCardWidget(level1CoverTexture, level1TitleTexture, level1BestTexture, level1WorldTexture);
 
-        level2 = new Image(new TextureRegionDrawable(new TextureRegion(levelTexture)));
-        setPositionRelative(1.0f - 0.083f, 0.65f, level2);
-        getStage().addActor(level2);
-
-        level3 = new Image(new TextureRegionDrawable(new TextureRegion(levelTexture)));
-        setPositionRelative(0.083f, 0.25f, level3);
-        getStage().addActor(level3);
-
-        level4 = new Image(new TextureRegionDrawable(new TextureRegion(levelTexture)));
-        setPositionRelative(1.0f - 0.083f, 0.25f, level4);
-        getStage().addActor(level4);
+        addRow(level1);
+        addRow(level2);
+        addRow(level3);
+        addRow(level4);
     }
 
     @Override
     public void dispose() {
-        levelTexture.dispose();
+        level1CoverTexture.dispose();
+        level1TitleTexture.dispose();
+        level1BestTexture.dispose();
+        level1WorldTexture.dispose();
         super.dispose();
     }
 
---
+++ core/src/com/jmolina/orb/widgets/LevelCardWidget.java
@@ -0,0 +1,52 @@
+package com.jmolina.orb.widgets;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Disposable;
+import com.jmolina.orb.groups.BaseGroup;
+import com.jmolina.orb.var.Vars;
+
+public class LevelCardWidget extends BaseGroup implements Disposable {
+
+    private Image cover;
+    private Image title;
+    private Image best;
+    private Image world;
+    private Image background;
+    private Image lock;
+
+    private Texture backgroundTexture;
+    private Texture lockTexture;
+
+    public LevelCardWidget(Texture coverTexture, Texture titleTexture, Texture bestTexture, Texture worldTexture) {
+        super();
+
+        backgroundTexture = new Texture(Gdx.files.internal("card_background.png"));
+
+        cover = new Image(coverTexture);
+        title = new Image(titleTexture);
+        best = new Image(bestTexture);
+        world = new Image(worldTexture);
+        background = new Image(backgroundTexture);
+
+        background.setPosition(0f, 0f);
+        cover.setPosition(0f, 0F);
+        title.setPosition(5.0f * Vars.GRID_UNIT, 3.0f * Vars.GRID_UNIT);
+        best.setPosition(5.0f * Vars.GRID_UNIT, 0.75f * Vars.GRID_UNIT);
+        world.setPosition(5.0f * Vars.GRID_UNIT, 0.0f);
+
+        addActor(background);
+        addActor(cover);
+        addActor(title);
+        addActor(best);
+        addActor(world);
+
+        setHeight(4.0f * Vars.GRID_UNIT);
+    }
+
+    @Override
+    public void dispose() {
+        backgroundTexture.dispose();
+    }
+}
