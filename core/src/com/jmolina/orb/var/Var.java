ce56730f7cc4973347fbf28ac96c428a32b2ab74
molinan2 <molinan2@gmail.com>
2016-06-30 14:38:24 +0200

    Deleted HTML project

--- core/src/com/jmolina/orb/var/Var.java
+++ core/src/com/jmolina/orb/var/Var.java
@@ -3,7 +3,7 @@ package com.jmolina.orb.var;
 public abstract class Var {
 
     public static final String APP_NAME = "Orb";
-    public static final String APP_VERSION = "0.4";
+    public static final String APP_VERSION = "0.5";
     public static final String AUTHOR = "Juan M. Molina";
     public static final int HEIGHT = 1184;
     public static final int WIDTH = 768;
