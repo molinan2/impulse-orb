31f6521eb6056b11a4cf2b95b1e4ebb744adf4b1
molinan2 <molinan2@gmail.com>
2016-08-15 19:31:44 +0200

    Fix: start level MAIN

--- core/src/com/jmolina/orb/var/Var.java
+++ core/src/com/jmolina/orb/var/Var.java
@@ -4,7 +4,7 @@ import com.jmolina.orb.managers.ScreenManager;
 
 public class Var {
 
-    public static final ScreenManager.Key FIRST_SCREEN = ScreenManager.Key.LEVEL_5;
+    public static final ScreenManager.Key FIRST_SCREEN = ScreenManager.Key.MAIN;
 
     public static final boolean DEBUG_INVULNERABILITY = false;
     public static final boolean DEBUG_RENDERER = false;
